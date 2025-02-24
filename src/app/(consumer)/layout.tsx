6a15310dcc06c5741f9ae679389d84bb5cb849dd
TheSungwon <thesungwon@gmail.com>
2025-02-25 00:32:33 +0900

    implement user management features and add utility functions; update TypeScript configuration and add Clerk webhook support

--- src/app/(consumer)/layout.tsx
+++ src/app/(consumer)/layout.tsx
@@ -1,5 +1,7 @@
+import { Button } from "@/components/ui/button";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 import Link from "next/link";
-import { ReactNode } from "react";
+import { ReactNode, Suspense } from "react";
 
 export default function ConsumerLayout({
   children,
@@ -16,9 +18,55 @@ function Navbar() {
   return (
     <header className="flex h-12 shadow bg-background z-10">
       <nav className="flex gap-4 container">
-        <Link className="mr-auto text-lg hover:underline px-2" href="/">
+        <Link
+          className="mr-auto text-lg hover:underline px-2 flex items-center"
+          href="/"
+        >
           Web
         </Link>
+        <Suspense fallback={<div>Loading...</div>}>
+          <SignedIn>
+            <Link
+              className="hover:bg-accent/10 flex items-center px-2"
+              href="/admin"
+            >
+              Admin
+            </Link>
+            <Link
+              className="hover:bg-accent/10 flex items-center px-2"
+              href="/courses"
+            >
+              My Course
+            </Link>
+            <Link
+              className="hover:bg-accent/10 flex items-center px-2"
+              href="/purchases"
+            >
+              Purchase History
+            </Link>
+            <div className="size-8 self-center">
+              <UserButton
+                appearance={{
+                  elements: {
+                    userButtonAvatarBox: {
+                      width: "100%",
+                      height: "100%",
+                    },
+                  },
+                }}
+              />
+            </div>
+          </SignedIn>
+          {/* 로그인 했을 때만 보임 */}
+        </Suspense>
+        <Suspense fallback={<div>Loading...</div>}>
+          <SignedOut>
+            <Button className="self-center" asChild>
+              <SignInButton>Sign In</SignInButton>
+            </Button>
+          </SignedOut>
+          {/* 로그인 안했을 때만 보임 */}
+        </Suspense>
       </nav>
     </header>
   );
