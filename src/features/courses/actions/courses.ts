9d37ee7dbcd1b5f73de9926e7c1dd204ef6d503a
TheSungwon <thesungwon@gmail.com>
2025-03-01 23:45:42 +0900

    코스 생성 기능 개선; 코스 스키마에서 불필요한 필드 제거 및 타입 정의 추가

--- src/features/courses/actions/courses.ts
+++ src/features/courses/actions/courses.ts
@@ -1,19 +1,19 @@
 "use server";
 
-// import { courseSchema } from "../schemas/courses";
+import { courseSchema } from "../schemas/courses";
 import { redirect } from "next/navigation";
 import { getCurrentUser } from "@/services/clerk";
 import { z } from "zod";
 import { canCreateCourses } from "../permissions/courses";
 import { insertCourse } from "../db/courses";
 
-const courseSchema = z.object({
-  id: z.string(),
-  name: z.string(),
-  description: z.string(),
-  createdAt: z.date(),
-  updatedAt: z.date(),
-});
+interface Course {
+  id: string;
+  name: string;
+  description: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
 
 export async function createCourse(unsafeData: z.infer<typeof courseSchema>) {
   const { success, data } = courseSchema.safeParse(unsafeData);
@@ -22,7 +22,7 @@ export async function createCourse(unsafeData: z.infer<typeof courseSchema>) {
     return { error: true, message: "There was an error creating your course" };
   }
 
-  const course = await insertCourse(data);
+  const course = await insertCourse(data as Course);
   console.log(course, "------------features/courses/actions/courses");
 
   redirect(`/admin/courses/${course.id}edit`);
--- src/features/courses/schemas/courses.ts
+++ src/features/courses/schemas/courses.ts
@@ -3,7 +3,4 @@ import { z } from "zod";
 export const courseSchema = z.object({
   name: z.string().min(1, "이름 입력해"),
   description: z.string().min(1, "설명 입력해"),
-  // id: z.string().min(1),
-  // createdAt: z.date(),
-  // updatedAt: z.date(),
 });
