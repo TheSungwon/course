571d536b96d4240d8bb205553c91149f6ba28d9a
박성원 <sw2park@gridone.co.kr>
2025-03-12 18:54:00 +0900

    Refactor lesson insertion and update functions; improve transaction handling and error management

--- src/features/lessons/db/lessons.ts
+++ src/features/lessons/db/lessons.ts
@@ -3,7 +3,6 @@ import { CourseSectionTable, LessonTable } from "@/drizzle/schema";
 import { eq } from "drizzle-orm";
 import { revalidateLessonCache } from "./cache/lessons";
 
-
 export async function getNextLessonOrder(sectionId: string) {
   const lesson = await db.query.LessonTable.findFirst({
     columns: { order: true },
@@ -14,40 +13,64 @@ export async function getNextLessonOrder(sectionId: string) {
   return lesson ? lesson.order + 1 : 0;
 }
 
-
-export async function insertLesson(data: typeof LessonTable.$inferInsert){
-  const [newLesson, courseId] =
-  await db.transaction(async trx => {
-    const [[newLesson], section] = 
-    await Promise.all([trx.insert(LessonTable).values(data).returning(),
-        trx.query.CourseSectionTable.findFirst({
-          columns:{courseId:true},
-          where: eq(CourseSectionTable.id, data.sectionId),
-        })
+export async function insertLesson(data: typeof LessonTable.$inferInsert) {
+  const [newLesson, courseId] = await db.transaction(async (trx) => {
+    const [[newLesson], section] = await Promise.all([
+      trx.insert(LessonTable).values(data).returning(),
+      trx.query.CourseSectionTable.findFirst({
+        columns: { courseId: true },
+        where: eq(CourseSectionTable.id, data.sectionId),
+      }),
     ]);
 
-    if(section == null) return trx.rollback();
+    if (section == null) return trx.rollback();
 
     return [newLesson, section.courseId];
-  })
+  });
 
-  if(newLesson == null) throw new Error("레슨 생성 실패");
+  if (newLesson == null) throw new Error("레슨 생성 실패");
 
-  revalidateLessonCache({courseId, id:newLesson.id});
+  revalidateLessonCache({ courseId, id: newLesson.id });
 
   return newLesson;
 }
-export async function updateLesson(id:string, data: Partial<typeof LessonTable.$inferInsert>){
+export async function updateLesson(
+  id: string,
+  data: Partial<typeof LessonTable.$inferInsert>
+) {
+  const [updatedLesson, courseId] = await db.transaction(async (trx) => {
+    const currentLesson = await trx.query.LessonTable.findFirst({
+      columns: { sectionId: true },
+      where: eq(LessonTable.id, id),
+    });
 
-  const [updatedLesson, courseId] =
-  await db.transaction(async trx => {
+    if (
+      data.sectionId != null &&
+      currentLesson?.sectionId !== data.sectionId &&
+      data.order == null
+    ) {
+      data.order = await getNextLessonOrder(data.sectionId);
+    }
 
-   
-  })
+    const [updatedLesson] = await trx
+      .update(LessonTable)
+      .set(data)
+      .where(eq(LessonTable.id, id))
+      .returning();
 
-  if(updatedLesson == null) throw new Error("레슨 생성 실패");
+    if (updatedLesson == null) throw new Error("레슨 수정 실패");
 
-  revalidateLessonCache({courseId, id:updatedLesson.id});
+    const section = await trx.query.CourseSectionTable.findFirst({
+      columns: { courseId: true },
+      where: eq(CourseSectionTable.id, updatedLesson.sectionId),
+    });
+
+    if (section == null) return trx.rollback();
+
+    return [updatedLesson, section.courseId];
+  });
+
+  revalidateLessonCache({ courseId, id: updatedLesson.id });
 
   return updatedLesson;
 }
